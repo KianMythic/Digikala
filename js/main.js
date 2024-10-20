783c55b0feda0c2f4b33d55f33ebd8e1a64185b5
Kian <kiankarami.09@gmail.com>
2024-10-21 02:08:02 +0330

    first commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 12.5,
+    spaceBetween: 1,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".mySlider", {
+    spaceBetween: 0,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".myOffer", {
+    slidesPerView: 8.1,
+    spaceBetween: 3,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".myFavorite", {
+    slidesPerView: 4,
+    spaceBetween: 1,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+
+
+  var swiper = new Swiper(".myBrand", {
+    slidesPerView: 9.25,
+    spaceBetween: 1,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
